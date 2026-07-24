873629b1e92d1e826b7ee9c231bfc061eb019ac8
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2026-07-25 00:39:57 +0600

    Data Insert in single and multiple row

---
+++ data-insert-single-multiple-row.ts
@@ -0,0 +1,21 @@
+//##Insert Single Row Data
+//insert into table_name (column1, column2, column3, ...)
+//values (value1, value2, value3, ...);
+// example:
+//insert into students(id, username, email, age, isActive)
+// values(
+//   1, 'shuvo', 'shuvo182@gmail.com', 18, true
+// )
+
+
+//##Insert Multiple Row Data
+//insert into table_name (column1, column2, column3, ...)
+//values (value1, value2, value3, ...),
+//       (value1, value2, value3, ...),
+//       (value1, value2, value3, ...);
+// example:
+// insert into students(id, username, email, age, isActive)
+// values
+//   (1, 'shuvo', 'shuvo182@gmail.com', 18, true),
+//   (2, 'alice', 'alice@example.com', 20, true),
+//   (3, 'bob', 'bob@example.com', 22, false);
