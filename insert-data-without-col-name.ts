5425d4909cd7d5406373ab2d11b00fd5b142673c
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2026-09-15 19:24:34 +0600

    Insrt data without col name

---
+++ insert-data-without-col-name.ts
@@ -0,0 +1,9 @@
+//insert data without column name
+
+//insert into table_name
+//values (value1, value2, value3, ...);
+// example:
+//insert into students
+// values(
+//   1, 'shuvo', 'shuvo@example.com', 18, true
+// )
