8ec08749bbc8e4ff206c9b918dce537ce8c6f4f6
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2026-09-15 21:14:35 +0600

    Alter Table

---
+++ alter-table.ts
@@ -0,0 +1,28 @@
+//ALTER TABLE
+
+//ALTER TABLE table_name
+//RENAME TO new_table_name;
+
+//ALTER TABLE table_name
+//ADD column_name datatype;
+
+//ALTER TABLE table_name
+//DROP COLUMN column_name;
+
+//ALTER TABLE table_name
+//RENAME COLUMN old_column_name TO new_column_name;
+
+//ALTER TABLE table_name
+//ALTER COLUMN column_name SET DATA TYPE new_data_type;
+
+//ALTER TABLE table_name
+//ALTER COLUMN column_name SET DEFAULT default_value;
+
+//ALTER TABLE table_name
+//ALTER COLUMN column_name DROP DEFAULT;
+
+//ALTER TABLE table_name
+//ADD CONSTRAINT constraint_name UNIQUE (column1, column2, ...);
+
+//ALTER TABLE table_name
+//DROP CONSTRAINT constraint_name;
