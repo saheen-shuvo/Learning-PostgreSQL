44931a39bd371c6cf136cc015ebf8947e1471473
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2026-09-17 19:12:27 +0600

    AND & OR

---
+++ and-or.ts
@@ -0,0 +1,7 @@
+//AND: The AND operator is used to combine multiple conditions in a WHERE clause. It returns true only if all the conditions are true.
+
+//select * from users where country = 'USA' and age < 25;
+
+//OR: The OR operator is used to combine multiple conditions in a WHERE clause. It returns true if at least one of the conditions is true.
+
+//select * from users where first_name like 'J%' or last_name like '%son';
