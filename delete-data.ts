58acd587a3043daa132bd379663bcb4651222809
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2026-09-18 12:07:42 +0600

    Delete & Update data

---
+++ delete-data.ts
@@ -0,0 +1,5 @@
+//Delete
+
+//delete from students where id = 1; // Deletes the student with id 1 from the students table
+
+//delete from students where email is null; // Deletes all students with a null email from the students table
---
+++ update-data.ts
@@ -0,0 +1,5 @@
+//Update
+
+//update students set email = 'shuvo@gmail.com' where id = 1; // Updates the email of the student with id 1 to '
+
+//update students set email = 'hi@gmail.com' where email is null; // Updates the email of all students with a null email to '
